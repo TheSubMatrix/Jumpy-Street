8e00b2ae9811fafe8f430f0774a81eec478690d8
bcollins87 <becollins@my.waketech.edu>
2026-01-07 19:13:12 -0500

    Saved Menu Scene

--- Assets/Scripts/MenuUIScripts/MenuScript.cs
+++ Assets/Scripts/MenuUIScripts/MenuScript.cs
@@ -12,10 +12,20 @@ public class MenuScript : MonoBehaviour
     public Button creditsButton;
     public Button quitButton;
 
+    public GameObject menuScreen;
+    public GameObject optionsScreen;
+    public GameObject controlsScreen;
+    public GameObject creditsScreen;
+    public GameObject playerUI;
+
     public Scene currentScene;
     void Start()
     {
-       
+        menuScreen.SetActive(true);
+        optionsScreen.SetActive(false);
+        controlsScreen.SetActive(false);
+        creditsScreen.SetActive(false);
+        playerUI.SetActive(false);
     }
 
 
@@ -26,21 +36,33 @@ public class MenuScript : MonoBehaviour
 
     void OnClickStart()
     {
-        
+        playerUI.SetActive(true);
     }
 
     void OnClickOptions()
     {
-
+        menuScreen.SetActive(false);
+        optionsScreen.SetActive(true);
+        controlsScreen.SetActive(false);
+        creditsScreen.SetActive(false);
+        playerUI.SetActive(false);
     }
 
     void OnClickCredits()
     {
-
+        menuScreen.SetActive(false);
+        optionsScreen.SetActive(false);
+        controlsScreen.SetActive(true);
+        creditsScreen.SetActive(false);
+        playerUI.SetActive(false);
     }
 
     void OnClickControls()
     {
-
+        menuScreen.SetActive(false);
+        optionsScreen.SetActive(false);
+        controlsScreen.SetActive(true);
+        creditsScreen.SetActive(false);
+        playerUI.SetActive(false);
     }
 }
