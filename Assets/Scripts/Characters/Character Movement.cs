0f6c5fbb9fe884a669279cb1dbd63b1f56236de9
Colin Whiteford <colinwhiteford516@gmail.com>
2026-01-09 00:45:25 -0500

    Cleaned Up Movement Moving Ground Logic

--- Assets/Scripts/Characters/Character Movement.cs
+++ Assets/Scripts/Characters/Character Movement.cs
@@ -18,37 +18,27 @@ public class CharacterMovement : MonoBehaviour
     bool m_isJumping;
     Coroutine m_currentMoveRoutine;
 
-    void Awake()
+    void OnEnable()
     {
         m_characterController.Initialize();
         m_characterController.RequestMovement += OnMoveRequested;
         m_rigidbody = GetComponent<Rigidbody>();
     }
-
-    void OnDestroy()
+    
+    void OnDisable()
     {
         m_characterController.RequestMovement -= OnMoveRequested;
         m_characterController.DeInitialize();
     }
 
-    void OnCollisionEnter(Collision collision)
-    {
-        if (m_isJumping || !IsGroundCollision(collision)) return;
-        m_isGrounded = true;
-        m_connectedMovingGround = collision.rigidbody;
-    }
-
-    void OnCollisionStay(Collision collision)
-    {
-        if (m_isJumping || !IsGroundCollision(collision)) return;
-        m_isGrounded = true;
-        m_connectedMovingGround = collision.rigidbody;
-    }
-
     void OnCollisionExit(Collision collision)
     {
-        if (IsGroundCollision(collision)) m_isGrounded = false;
-        if (collision.rigidbody == m_connectedMovingGround) m_connectedMovingGround = null;
+        if (!IsGroundCollision(collision)) return;
+        m_isGrounded = false;
+        if (collision.rigidbody == m_connectedMovingGround)
+        {
+            m_connectedMovingGround = null;
+        }
     }
 
     static bool IsGroundCollision(Collision collision)
@@ -62,34 +52,36 @@ public class CharacterMovement : MonoBehaviour
         m_currentMoveRoutine = StartCoroutine(MoveCharacter(movementVector));
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        if (!IsGroundCollision(collision)) return;
+        m_isGrounded = true;
+        m_connectedMovingGround = collision.rigidbody;
+    }
+
     IEnumerator MoveCharacter(Vector2 movementVector)
     {
         m_isGrounded = false;
-        m_isJumping = true;
-        
         float elapsedTime = 0;
         Vector3 startPosition = m_rigidbody.position;
         Vector3 movementOffset = new(movementVector.normalized.x * m_moveDistance, 0, movementVector.normalized.y * m_moveDistance);
-        
         Rigidbody movingGround = m_connectedMovingGround;
         Vector3 platformStartPos = movingGround ? movingGround.position : Vector3.zero;
-        
         while(elapsedTime <= m_moveTime)
         {
             float t = elapsedTime / m_moveTime;
             Vector3 movingGroundDelta = movingGround ? movingGround.position - platformStartPos : Vector3.zero;
             Vector3 targetPosition = startPosition + movementOffset * t + movingGroundDelta;
             targetPosition.y += Mathf.Sin(t.Remap(0, 1, 0, Mathf.PI)) * m_jumpHeight;
-            
+    
             m_rigidbody.MovePosition(targetPosition);
             elapsedTime += Time.fixedDeltaTime;
             yield return new WaitForFixedUpdate();
         }
-        
+
         Vector3 finalPlatformDelta = movingGround ? movingGround.position - platformStartPos : Vector3.zero;
         m_rigidbody.MovePosition(startPosition + movementOffset + finalPlatformDelta);
-        
-        m_isJumping = false;
+
         m_currentMoveRoutine = null;
     }
 }
