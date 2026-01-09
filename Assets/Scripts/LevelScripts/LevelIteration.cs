22f789de1ad970d0eeb84bdcd62388cfd68972dd
bcollins1987 <becollins@my.waketech.edu>
2026-01-09 12:35:02 -0500

    Level Iteration Works
    The code for the level iteration works, when the player collides with the tile collider the next tile loads, the collider then is set to inactive, moves forward a certain amount and then reactivates. The collider is now a separate game object and not included with the tile prefab

--- Assets/Scripts/LevelScripts/LevelIteration.cs
+++ Assets/Scripts/LevelScripts/LevelIteration.cs
@@ -4,20 +4,16 @@ public class LevelIteration : MonoBehaviour
 {
     public GameObject baseTile;
     public GameObject tileTrigger;
-    public GameObject targetTrigger;
-    private int tileNumber = 0;
-    //public GameObject targetTrigger;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("TileTrig"))
         {
             Vector3 tile = other.transform.position;
             Instantiate(baseTile, new Vector3(tile.x, tile.y, tile.z + 50), Quaternion.identity);
-            if (tileNumber >= 1) 
-            {  
-                
-            }
-            tileNumber++;
+            tileTrigger.SetActive(false);
+            tileTrigger.transform.position = new Vector3(tile.x, tile.y, tile.z + 50);
+            tileTrigger.SetActive(true);
         }
 
        /* if (other.gameObject.CompareTag("TileTrig"))
